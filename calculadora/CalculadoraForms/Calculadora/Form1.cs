4cb3a6791609225e332bfc25b8b5e6be59e3eed9
Jenifer Sousa <jenifferpsousa@gmail.com>
2022-03-18 16:56:39 -0300

    projeto academico - calculadora main

---
+++ calculadora/CalculadoraForms/Calculadora/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt16(textBox1.Text);
+            valor2 = Convert.ToInt16(textBox2.Text);
+
+            resultado = valor1 + valor2;
+
+            label3.Text = Convert.ToString(resultado);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt16(textBox1.Text);
+            valor2 = Convert.ToInt16(textBox2.Text);
+
+            resultado = valor1 - valor2;
+
+            label3.Text = Convert.ToString(resultado);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          float valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt16(textBox1.Text);
+            valor2 = Convert.ToInt16(textBox2.Text);
+
+            resultado = valor1 / valor2;
+
+            label3.Text = Convert.ToString(resultado);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt16(textBox1.Text);
+            valor2 = Convert.ToInt16(textBox2.Text);
+
+            resultado = valor1 * valor2;
+
+            label3.Text = Convert.ToString(resultado);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label3.Text = (" ");
+        }
+    }
+}
---
+++ calculadora/CalculadoraSimples/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+double valor1, valor2, soma,
+    subtracao, divisao, multi;
+string nome;
+
+Console.WriteLine("Digite seu nome");
+nome = Console.ReadLine();
+Console.WriteLine("Bem vindo"+ " " + nome );
+
+Console.WriteLine("Digite 2 valores para continuar - Adicione o 1º valor:");
+valor1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Adicione o 2º valor:");
+valor2 = Convert.ToDouble(Console.ReadLine());
+
+soma = valor1 + valor2;
+subtracao = valor1 - valor2;
+divisao  = valor1 / valor2;
+multi  = valor1 * valor2;
+
+Console.WriteLine("A soma é:" + soma);
+Console.WriteLine("A subtração é:" + subtracao);
+Console.WriteLine("A divisão é:" + divisao);
+Console.WriteLine("A multiplicação é:" + multi);
+
